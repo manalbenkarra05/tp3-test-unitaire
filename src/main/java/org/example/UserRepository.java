c364a9ecdd035788ac440af91ea50b934d28cec1
manalbenkarra05 <benkarramanal@gmail.com>
2025-04-18 23:45:08 +0100

    Initial commit - TP3 tests d’intégration avec JUnit & Mockito exo1

---
+++ src/main/java/org/example/UserRepository.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public interface UserRepository {
+    User findUserById(long id);
+}
+
---
+++ src/test/java/UserServiceTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.example.*;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceTest {
+
+    @Test
+    public void testGetUserById() {
+        UserRepository mockRepo = mock(UserRepository.class);
+        User expectedUser = new User(1L, "manal");
+        when(mockRepo.findUserById(1L)).thenReturn(expectedUser);
+
+        UserService userService = new UserService(mockRepo);
+        User result = userService.getUserById(1L);
+
+        assertNotNull(result);
+        assertEquals("manal", result.getName());
+        verify(mockRepo).findUserById(1L);
+    }
+}
